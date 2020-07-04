c48c9bc3257ea22d1839a107dbcf20e0e8ac23bc
Manna <manna23.sandhu@gmail.com>
2020-07-04 13:06:15 +0530

    added settings

--- components/AppDrawerNavigator.js
+++ components/AppDrawerNavigator.js
@@ -2,11 +2,15 @@ import React from 'react';
 import {createDrawerNavigator} from 'react-navigation-drawer';
 import {AppTabNavigator} from './AppTabNavigator';
 import CustomSideBarMenu from './CustomSlideBar';
+import SettingScreen from '../screens/SettingScreen';
 
 export const AppDrawerNavigator = createDrawerNavigator({
   Home : {
     screen : AppTabNavigator
-    },
+  },
+  Settings: {
+    screen: SettingScreen
+  }, 
   },
   {
     contentComponent:CustomSideBarMenu
